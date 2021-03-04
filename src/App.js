3da5540ef502ff7f54610b8ec879191e14af7fc6
Jianyuan Chen <jianyuan.chen12@gmail.com>
2021-03-03 23:42:43 -0800

    Setup SPA

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/Nav/Navbar';
+import Description from './components/Description/Description';
+import Zen from './components/Zen/Zen';
+import ZenTitle from './components/ZenTitle/ZenTitle';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      // <Switch>
+      //   <Route
+      //     exact
+      //     path="/"
+      //     render={() => {
+      <div className="App">
+        <Navbar />
+        <Description />
+        <ZenTitle />
+        <Zen />
+      </div>
+      //   }}
+      // />
+    );
+  }
+}
+
+export default App;
